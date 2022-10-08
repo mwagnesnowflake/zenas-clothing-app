6c65edc63deec542a52da45db0dcfa89ef06be7a
mwagnesnowflake <114114013+mwagnesnowflake@users.noreply.github.com>
2022-10-07 22:14:17 -0500

    Update zenas_app_code.py

--- zenas_app_code.py
+++ zenas_app_code.py
@@ -21,3 +21,17 @@ option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list
 
 #add caption
 product_caption = 'Our warm, comfortable, '+ option +' sweatsuit!'
+
+#use the option to retrieve additional data from snowflake
+my_cur.execute("select DIRECT_URL, PRICE, SIZE_LIST, UPSELL_PRODUCT_DESC FROM ZENAS_ATHLEISURE_DB.PRODUCTS.CATALOG_FOR_WEBSITE WHERE COLOR_OR_STYLE = "'+ option +'";")
+df2 = my_cur.fetchone()
+streamlit.write(df2)
+
+streamlit.image(
+    df2[0],
+    width=400,
+    caption=product_caption
+)
+streamlit.write('Price: ', df2[1])
+streamlit.write('Sizes Available: ',df2[2])
+streamlit.write(df2[3])
