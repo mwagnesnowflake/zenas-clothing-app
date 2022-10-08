c6756e24de94f7b9b192f32b6dcc69ea621674f3
mwagnesnowflake <114114013+mwagnesnowflake@users.noreply.github.com>
2022-10-07 21:59:52 -0500

    Update zenas_app_code.py

--- zenas_app_code.py
+++ zenas_app_code.py
@@ -5,10 +5,13 @@ import snowflake.connector
 #top heading banner
 streamlit.title("Zena's Amazing Athleisure Catalog")
 
+#get the snowflake data
 def get_sweats_info():
-  with my_cnx.cursor() as my_cur:
-    my_cur.execute("select COLOR_OR_STYLE from ZENAS_ATHLEISURE_DB.PRODUCTS.CATALOG_FOR_WEBSITE")
-                   return my_cur.fetchall()
+with my_cnx.cursor() as my_cur:
+my_cur.execute("select COLOR_OR_STYLE from ZENAS_ATHLEISURE_DB.PRODUCTS.CATALOG_FOR_WEBSITE")
+return my_cur.fetchall()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-                   my_data_rows = get_sweats_info()
-                   my_cnx.close()
+my_data_rows = get_sweats_info()
+my_cnx.close()
+
+df = pands.DataFrame(my_data_rows)
